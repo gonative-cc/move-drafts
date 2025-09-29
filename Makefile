4f3a0aed55277b3ec12ae174cf9d6a17b8a2d1e3
Robert Zaremba <robert@zaremba.ch>
2025-09-29 18:43:00 +0200

    setup: add configs and Makefiles
    Signed-off-by: Robert Zaremba <robert@zaremba.ch>

---
+++ Makefile
@@ -0,0 +1,45 @@
+
+# All immediate subdirectories inside /packages
+SUBDIRS := $(wildcard */)
+MAKE_SUBDIRS := $(foreach dir,$(SUBDIRS),$(if $(wildcard $(dir)Makefile),$(dir)))
+MOVE_SUBDIRS := $(foreach dir,$(SUBDIRS),$(if $(wildcard $(dir)Move.toml),$(dir)))
+
+
+.PHONY: setup-hooks lint-git
+
+setup-hooks:
+	@cd .git/hooks; ln -s -f ../../contrib/git-hooks/* ./
+	@bun install -g prettier @mysten/prettier-plugin-move
+
+.git/hooks/pre-commit: setup
+
+
+# used as pre-commit
+lint-git:
+	@git diff --name-only --cached --diff-filter=ACM | grep  -E '\.md$$' | xargs -r markdownlint-cli2
+
+# add license header to every source file
+add-license:
+	@awk -i inplace 'FNR==1 && !/SPDX-License-Identifier/ {print "// SPDX-License-Identifier: MPL-2.0\n"}1' **/sources/*.move **/tests/*.move
+# with reuse tool:
+# docker run --rm --volume $(pwd):/data fsfe/reuse annotate --license MPL-2.0  */tests/*.move */sources/*.move  -s cpp
+
+.PHONY: add-license
+
+
+############
+
+print-packages:
+	@for p in $(MAKE_SUBDIRS); do \
+		echo $$p; done
+
+format-move-all:
+	@prettier-move -w */sources/* */tests/*
+
+test-move-all:
+	bash ./contrib/run-move-tests.sh test
+
+build-move-all:
+	bash ./contrib/run-move-tests.sh build
+
+.PHONY: print-packages format-move build-all test-all
---
+++ Makefile-common.mk
@@ -0,0 +1,55 @@
+
+# lint changed files
+lint:
+	@git diff --name-only | grep  -E '\.md$$' | xargs -r markdownlint-cli2
+	@sui move build --lint
+
+lint-all:
+	@markdownlint-cli2 **.md
+	@sui move build --lint
+
+lint-fix-all:
+	@markdownlint-cli2 --fix **.md
+	@echo "Sui move lint will be fixed by manual"
+	@bun run format:move-all
+
+format-fix:
+	@prettier-move -w sources/* tests/*
+
+.PHONY: setup lint-git lint lint-all lint-fix-all format-fix
+
+
+###############################################################################
+##                              Build & Test                                 ##
+###############################################################################
+
+build:
+	@sui move build
+
+test:
+	@sui move test
+
+test-coverage:
+	echo TODO
+# sui move test --coverage
+# sui move coverage
+
+.PHONY: build test test-coverage
+
+###############################################################################
+##                                   Docs                                    ##
+###############################################################################
+# Variables for build output and module name
+BUILD_DIR := build
+PACKAGE_NAME := $(notdir $(CURDIR))
+gen-docs:
+	@sui move build --doc
+	@cp -r ./$(BUILD_DIR)/$(PACKAGE_NAME)/docs/$(PACKAGE_NAME)/* ./docs
+
+.PHONY: gen-docs
+
+###############################################################################
+##                                Infrastructure                             ##
+###############################################################################
+
+# To setup bitcoin, use Native Relayer.
